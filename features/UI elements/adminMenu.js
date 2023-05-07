684a7ca53aa1eb0c21c6af9e64bc08cfe15c27ce
julianpuentesuribe <w.puentes@uniandes.edu.co>
2023-05-07 01:12:37 -0500

    Merge escenario P15

--- features/UI elements/adminMenu.js
+++ features/UI elements/adminMenu.js
@@ -8,14 +8,14 @@ class AdminMenu {
   get postTab() {
     return this.driver.$("a[href='#/posts/']");
   }
-  get tagTab() {
-    return this.driver.$("a[href='#/tags/']");
-  }
   get staffTab() {
     return this.driver.$("a[href='#/staff/']");
   }
   get designTab() {
     return this.driver.$("a[href='#/settings/design/']");
+  }
+    get tagTab() {
+    return this.driver.$("a[href='#/tags/']");
   }
 }
 
